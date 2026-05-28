f7cd83fd402f1812f044b17e27a4a901be1b0798
qar_ty <qar_ty11@protonmail.com>
2026-05-28 00:24:54 -0500

    coooooding

---
+++ test2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <float.h>
+#include <string.h>
+#include <any>
+using namespace std;
+
+
+
+int main() {
+
+	auto a = 2.0f;
+	if (a == 2.0f) {
+		while (<a100.0f) {
+		a = a + 1.0f;
+	}
+}
+
+	cout << a << " "  << "\n";
+
+
+    return 0;
+}
---
+++ test2.lua
@@ -0,0 +1,7 @@
+local a = 2
+if a == 2 then
+  while a < 100 do
+    a = a + 1
+  end
+end
+print(a)
